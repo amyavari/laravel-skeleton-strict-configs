7f208b8065bc35e7cf6b3f464503536c2a8a38af
Ali M. Yavari <ali.m.yavari70@gmail.com>
2025-12-18 10:13:10 +0330

    build: configure Laravel app for necessary restrictions

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +24,59 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configureUrl();
+        $this->configureDates();
+    }
+
+    /**
+     * Block destructive commands in production. Like:
+     *
+     * - db:wipe
+     * - migrate:fresh
+     * - migrate:refresh
+     * - migrate:reset
+     * - migrate:rollback
+     */
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            (bool) $this->app->environment('production')
+        );
+    }
+
+    /**
+     * Prevent dangerous behavior and be more restrictive.
+     *
+     * - Prevent lazy loading.
+     * - Prevent silently discarding attributes (in mass assignment).
+     * - Prevent accessing missing attributes.
+     * - Disable all mass assignable restrictions
+     * - Model relationships should be automatically eager loaded when accessed
+     */
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict();
+        Model::unguard();
+        Model::automaticallyEagerLoadRelationships();
+    }
+
+    /**
+     * Only handle HTTPS requests.
+     */
+    private function configureUrl(): void
+    {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+    }
+
+    /**
+     * Get new instance of date each time we modify it.
+     */
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
     }
 }
